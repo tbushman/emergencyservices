9cb5a663d4b6730124f5db19b932303815c9331b
tbushman <tbushman@pu.bli.sh>
2017-01-27 01:14:18 -0700

    remove favicon ref for now

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.locals.appTitle = 'emergencyservices';
-app.use(favicon(path.join(__dirname, 'public/img', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public/img', 'favicon.ico')));
 app.locals.moment = require('moment');
 app.locals.$ = require('jquery');
 //app.locals.L = require('leaflet');
