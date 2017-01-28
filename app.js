7b31f541ff17bae9d38dfc2e1cad27363de176f3
tbushman <tbushman@pu.bli.sh>
2017-01-28 02:26:21 -0700

    blergh

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', pug);
+app.set('view engine', 'pug');
 
 app.locals.appTitle = 'emergencyservices';
 app.use(favicon(path.join(__dirname, 'public/img', 'favicon.ico')));
