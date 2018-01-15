9622221c34720cec1a3db06811f05b41774d9715
tbushman <tbushman@pu.bli.sh>
2018-01-15 13:33:13 -0700

    replace req.app.locals with req.session omg

--- routes/index.js
+++ routes/index.js
@@ -325,7 +325,7 @@ router.get('/api/import', function(req, res, next){
 			return next(err)
 		}
 		return res.render('import', {
-			loggedin: req.app.locals.loggedin,
+			loggedin: req.session.loggedin,
 			data: ret,
 			info: ':)'
 		})
