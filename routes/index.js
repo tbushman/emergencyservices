c75c359effe6a4f766944637d7a97824fe5c6468
tbushman <tbushman@pu.bli.sh>
2019-12-30 08:18:06 -0700

    npm address

--- routes/index.js
+++ routes/index.js
@@ -355,15 +355,17 @@ router.all('/mydata/:zoom/:lat/:lng', function(req, res, next){
 });
 
 router.get('/near', async function(req, res, next){
-	var ip = require("ip");
-	var ping = spawn('ping', [ip.address()]);
-	ping.stdout.on('data', function(d){
-		console.log(d)
-	});
-	console.log ( ip.address(), req.headers );
-	const arp = require('arp');
-	const ipa = process.env.NODE_ENV === 'production' ? req.headers['X-Forwarded-For'] : ip.address();
-	arp.getMAC(ipa, (err, mac) => {
+	// var ip = require("ip");
+	// var ping = spawn('ping', [ip.address()]);
+	// ping.stdout.on('data', function(d){
+	// 	console.log(d)
+	// });
+	// console.log ( ip.address(), req.headers );
+	// const arp = require('arp');
+	const address = require('address');
+	// const ipa = process.env.NODE_ENV === 'production' ? req.headers['X-Forwarded-For'] : ip.address();
+	// arp.getMAC(ipa, (err, mac) => {
+	address.mac('vboxnet', (err, mac) => {
 		console.log(mac)
 		const params = {
 			wifiAccessPoints: [{
