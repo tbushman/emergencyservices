26b26421537b22638f52c02ed5c9977b07dd7605
tbushman <tbushman@pu.bli.sh>
2020-11-15 22:23:07 -0700

    sw today Time

---
+++ models/shelterwatch.js
@@ -0,0 +1,13 @@
+{
+	Date:Date,
+	Time:String,
+	"Geraldine King":Number,
+	"Mens Shelter":Number,
+	"Gail Miller":Number,
+	"Gail Miller (Women)":Number,
+	"Gail Miller (Men)":Number,
+	"Overflow Center":Number,
+	"Total (non-site-specific)":Number,
+	"Temperature @ Call Time (F)":Number,
+	"Daily Temp Low":Number
+}
